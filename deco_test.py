951744fe81d7266c2c05fac977d816e1817b747c
caihang <ch_snail@163.com>
2017-03-16 19:41:34 +0800

    deco_test.py add handle args

--- deco_test.py
+++ deco_test.py
@@ -18,7 +18,31 @@ def my_func():
     time.sleep(0.6)
     print 'my_func end'
 
+def deco_with_arg(arg = True):
+    if arg:
+        def _deco(func):
+            def wrapper(*args, **kwargs):
+                starttime = int(time.time())
+                func(*args, **kwargs)
+                endtime = int(time.time())
+                mesecs = (endtime - starttime)*1000
+                print "->elapsed time: %f ms"%mesecs
+            return wrapper
+    else:
+        def _deco(func):
+            return func
+    return _deco
+
+@deco_with_arg(True)
+def add_func(a, b):
+    print 'start add'
+    time.sleep(0.6)
+    print 'result is %d'%(a+b)
+    print 'end add'
+
 if __name__ == '__main__':
     print "my_func is: ", my_func.__name__
     print ''
     my_func()
+
+    add_func(4, 7)
